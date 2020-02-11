30ab85acf60ed4d39572b60b3814924a523a4c75
Nolan Picini <nolanpic@gmail.com>
2020-02-11 13:42:36 -0800

    Add GET /posts/:id

--- posts/router.js
+++ posts/router.js
@@ -15,4 +15,24 @@ router.get('/', (req, res) => {
     });
 });
 
+/**
+ * GET post by id
+ */
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+
+    DB.findById(id).then(post => {
+        if(!post.length) {
+            // 404 post not found
+            res.status(404).json({ message: "The post with the specified ID does not exist." });
+        }
+        else {
+            res.status(200).json(post);
+        }
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({ error: "The post information could not be retrieved." });
+    });
+});
+
 module.exports = router;
